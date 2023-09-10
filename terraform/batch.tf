407824ecbeab052322435e33c7b52f12f86750f9
Aurèle Oulès <aurele@oules.com>
2023-09-10 21:04:07 +0200

    Reduce timeout for mutation

--- terraform/batch.tf
+++ terraform/batch.tf
@@ -117,6 +117,6 @@ resource "aws_batch_job_definition" "mutation_job" {
     jobRoleArn       = aws_iam_role.job_role.arn
   })
   timeout {
-    attempt_duration_seconds = 2700 # 45 minutes
+    attempt_duration_seconds = 1800 # 30 minutes
   }
 }
