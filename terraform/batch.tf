5ced0080d115dff29f649e8aec45af54fd00bdf8
Aurèle Oulès <aurele@oules.com>
2023-09-10 15:37:25 +0200

    Increase compute max resources

--- terraform/batch.tf
+++ terraform/batch.tf
@@ -2,7 +2,7 @@ resource "aws_batch_compute_environment" "jobs_compute" {
   compute_environment_name = "jobs_compute"
 
   compute_resources {
-    max_vcpus = 8
+    max_vcpus = 32
 
     security_group_ids = [data.aws_security_group.default.id]
 
