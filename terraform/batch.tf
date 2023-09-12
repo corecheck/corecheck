a8475ed986c5972f06506dfb9c0d7ae84e0ce2f5
Aurèle Oulès <aurele@oules.com>
2023-09-12 16:00:51 +0200

    Reduce AWS batch timeout

--- terraform/batch.tf
+++ terraform/batch.tf
@@ -115,6 +115,6 @@ resource "aws_batch_job_definition" "mutation_job" {
     jobRoleArn       = aws_iam_role.job_role.arn
   })
   timeout {
-    attempt_duration_seconds = 3600
+    attempt_duration_seconds = 1800
   }
 }
