61f97feb748348f49b1ab29cb663106b4c65b7a2
Aurèle Oulès <aurele@oules.com>
2023-09-19 11:38:33 +0000

    Add SSH key

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -10,9 +10,15 @@ data "aws_ami" "ecs-optimized" {
   owners = ["amazon"]
 }
 
+resource "aws_key_pair" "ssh_key" {
+    key_name = "aureleoules"
+    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEM79mi/xHOtZw+bUfOH8soMjCyO5qOdpLls1tXnR2AD aurele@oules.com"
+}
+
 resource "aws_instance" "core" {
   instance_type               = "t4g.nano"
   associate_public_ip_address = true
 
   ami = data.aws_ami.ecs-optimized.id
+  key_name = aws_key_pair.ssh_key.key_name
 }
