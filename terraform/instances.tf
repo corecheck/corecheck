cf7b625c0fde94f3fa4eb3aed1c0371301b87084
Aurèle Oulès <aurele@oules.com>
2023-09-19 11:47:03 +0000

    EIP

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -15,9 +15,13 @@ resource "aws_key_pair" "ssh_key" {
     public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEM79mi/xHOtZw+bUfOH8soMjCyO5qOdpLls1tXnR2AD aurele@oules.com"
 }
 
+resource "aws_eip" "lb" {
+  instance = aws_instance.core.id
+  domain   = "vpc"
+}
+
 resource "aws_instance" "core" {
   instance_type               = "t4g.nano"
-  associate_public_ip_address = true
 
   ami = data.aws_ami.ecs-optimized.id
   key_name = aws_key_pair.ssh_key.key_name
