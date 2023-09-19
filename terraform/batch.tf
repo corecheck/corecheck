cad49b97433a2850ba2d85482a8d2e7f1d0e78de
Aurèle Oulès <aurele@oules.com>
2023-09-19 10:21:18 +0000

    Increase max_vcpus

--- terraform/batch.tf
+++ terraform/batch.tf
@@ -2,7 +2,7 @@ resource "aws_batch_compute_environment" "jobs_compute" {
   compute_environment_name = "jobs_compute"
 
   compute_resources {
-    max_vcpus = 64
+    max_vcpus = 128
 
     security_group_ids = [data.aws_security_group.default.id]
 
