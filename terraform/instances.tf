128bf04313f53f5aaf61e9efe0e481986963549b
Aurèle Oulès <aurele@oules.com>
2023-09-19 11:30:37 +0000

    Use ECS image

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,4 +1,18 @@
+# Amazon ECS-Optimized Amazon Linux 
+data "aws_ami" "ecs-optimized" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+  }
+
+  owners = ["amazon"]
+}
+
 resource "aws_instance" "core" {
   instance_type               = "t4g.nano"
   associate_public_ip_address = true
+
+  ami = data.aws_ami.ecs-optimized.id
 }
