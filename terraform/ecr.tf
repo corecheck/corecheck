3b68c3ce71a6cecf79cc2d69634f15bde377c2ad
Aurele Oules <aurele@oules.com>
2023-11-25 16:23:57 +0100

    Remove mutation jobs

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -7,8 +7,3 @@ resource "aws_ecrpublic_repository" "bitcoin-coverage-mutation-worker" {
   provider        = aws.us_east_1
   repository_name = "bitcoin-coverage-mutation-worker"
 }
-
-resource "aws_ecrpublic_repository" "bitcoin-coverage-coverage-worker" {
-  provider        = aws.us_east_1
-  repository_name = "bitcoin-coverage-coverage-worker"
-}
