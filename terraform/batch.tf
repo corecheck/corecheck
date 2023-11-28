8af64127ba07c4a27a3ccc44ac027123fe3ddebf
Aurele Oules <aurele@oules.com>
2023-11-28 19:31:16 +0100

    Restrict to only one AZ

--- terraform/batch.tf
+++ terraform/batch.tf
@@ -7,8 +7,6 @@ resource "aws_batch_compute_environment" "jobs_compute" {
 
     subnets = [
       data.aws_subnets.example.ids[0],
-      data.aws_subnets.example.ids[1],
-      data.aws_subnets.example.ids[2],
     ]
 
     type                = "SPOT"
