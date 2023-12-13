f8e01f626ae72a0f999a03803001b76afac9b132
Aurele Oules <aurele@oules.com>
2023-12-13 01:23:22 +0100

    Add get-pull function

---
+++ functions/get-pull/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	echoadapter "github.com/awslabs/aws-lambda-go-api-proxy/echo"
+	"github.com/corecheck/corecheck/internal/config"
+	"github.com/corecheck/corecheck/internal/db"
+	"github.com/corecheck/corecheck/internal/logger"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var (
+	cfg        = Config{}
+	log        = logger.New()
+	echoLambda *echoadapter.EchoLambda
+)
+
+func getPull(c echo.Context) error {
+	pullNumber := c.Param("number")
+	pullNumberInt, err := strconv.Atoi(pullNumber)
+	if err != nil {
+		log.Errorf("Error converting pull number to int: %s", err)
+		return err
+	}
+	pull, err := db.GetPR(pullNumberInt)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	reports, err := db.GetPullCoverageReports(pullNumberInt)
+	if err != nil {
+		log.Errorf("Error getting coverage reports: %s", err)
+		return err
+	}
+
+	for i := range reports {
+		reports[i].CoverageFiles = CreateCoverageFileHunks(reports[i].CoverageLines)
+		reports[i].BenchmarksGrouped = GroupBenchmarks(reports[i].Benchmarks)
+		reports[i].BaseReport, err = db.GetMasterCoverageReport(reports[i].BaseCommit)
+		reports[i].BaseReport.BenchmarksGrouped = GroupBenchmarks(reports[i].BaseReport.Benchmarks)
+
+		if err != nil {
+			log.Errorf("Error getting base report: %s", err)
+			continue
+		}
+	}
+
+	pull.Reports = reports
+
+	return c.JSON(200, pull)
+}
+
+func main() {
+	log.Debug("Loading config...")
+	if err := config.Load(&cfg); err != nil {
+		log.Fatalf("Error loading config: %s", err)
+	}
+
+	log.Debug("Connecting to database...")
+	if err := db.Connect(cfg.DatabaseConfig); err != nil {
+		log.Fatalf("Error connecting to database: %s", err)
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+		AllowHeaders: []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
+	}))
+	e.Use(middleware.Logger())
+
+	e.GET("/pulls/:number", getPull)
+
+	echoLambda = echoadapter.New(e)
+	handler := func(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		return echoLambda.ProxyWithContext(ctx, req)
+	}
+
+	lambda.Start(handler)
+}
--- go.mod
+++ go.mod
@@ -7,8 +7,10 @@ require (
 	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
 	github.com/aws/aws-lambda-go v1.41.0 // indirect
 	github.com/aws/aws-sdk-go v1.48.13 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.0 // indirect
 	github.com/cloudflare/circl v1.3.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-github v17.0.0+incompatible // indirect
 	github.com/google/go-github/v54 v54.0.0 // indirect
@@ -22,6 +24,12 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/labstack/echo/v4 v4.11.3 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/waigani/diffparser v0.0.0-20190828052634-7391f219313d // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
@@ -30,6 +38,7 @@ require (
 	golang.org/x/oauth2 v0.11.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
