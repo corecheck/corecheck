a3d778ef2471155b4fb41e60f5c1b2d8386d03c0
Aurele Oules <aurele@oules.com>
2023-12-16 21:30:50 +0100

    Add logs to handle-benchmarks

--- functions/compute/handle-benchmarks/main.go
+++ functions/compute/handle-benchmarks/main.go
@@ -37,15 +37,18 @@ func handleBenchmarkSuccess(job *types.JobParams) error {
 	}
 
 	for n := 0; n < cfg.BenchArraySize; n++ {
+		log.Infof("Getting benchmark data for n=%d", n)
 		var benchResults []*db.BenchmarkResult
 		var err error
 		if job.GetIsMaster() {
+			log.Info("Getting benchmark data for master")
 			benchResults, err = GetBenchDataMaster(job.Commit, n)
 			if err != nil {
 				log.Error("Error getting benchmark data", err)
 				return err
 			}
 		} else {
+			log.Info("Getting benchmark data for PR")
 			benchResults, err = GetBenchData(job.GetPRNumber(), job.Commit, n)
 			if err != nil {
 				log.Error("Error getting benchmark data", err)
@@ -53,14 +56,17 @@ func handleBenchmarkSuccess(job *types.JobParams) error {
 			}
 		}
 
+		log.Infof("Grouping benchmarks for n=%d", n)
 		totalBenchmarks = append(totalBenchmarks, benchResults...)
 	}
 
+	log.Info("Creating benchmark results")
 	err = db.CreateBenchmarkResults(report.ID, totalBenchmarks)
 	if err != nil {
 		log.Error("Error creating benchmark results", err)
 		return err
 	}
+	log.Info("Updating coverage report")
 
 	err = db.UpdateCoverageReport(report.ID, report.Status, db.BENCHMARK_STATUS_SUCCESS, report.CoverageRatio, report.BaseCommit)
 	if err != nil {
