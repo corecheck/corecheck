47a4f8c48a354320e5ef2cbf9eda8183cf3b7cf5
Aurele Oules <aurele@oules.com>
2023-12-23 14:27:42 +0100

    Remove unused code

--- functions/compute/handle-benchmarks/util.go
+++ functions/compute/handle-benchmarks/util.go
@@ -32,13 +32,3 @@ func getBenchData(url string) ([]*db.BenchmarkResult, error) {
 
 	return benchResults, nil
 }
-
-func GroupBenchmarks(benchmarks []db.BenchmarkResult) map[string][]db.BenchmarkResult {
-	groupedBenchmarks := make(map[string][]db.BenchmarkResult)
-
-	for _, benchmark := range benchmarks {
-		groupedBenchmarks[benchmark.Name] = append(groupedBenchmarks[benchmark.Name], benchmark)
-	}
-
-	return groupedBenchmarks
-}
