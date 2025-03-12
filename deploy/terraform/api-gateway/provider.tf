be6d700ef2f26ec9dbfaea8bd4c9b98a820b3ec9
Se7enZ <se7enz@protonmail.com>
2025-03-12 13:27:07 -0700

    alias and pass in Terraform providers to modules

--- deploy/terraform/api-gateway/provider.tf
+++ deploy/terraform/api-gateway/provider.tf
@@ -2,13 +2,9 @@ terraform {
   required_version = ">= 0.14.0"
   required_providers {
     aws = {
+      configuration_aliases = [aws.us_east_1]
       source                = "hashicorp/aws"
       version               = "5.15.0"
     }
   }
-}
-
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
 }
--- deploy/terraform/compute/provider.tf
+++ deploy/terraform/compute/provider.tf
@@ -2,16 +2,11 @@ terraform {
   required_version = ">= 0.14.0"
   required_providers {
     aws = {
-      configuration_aliases = [aws.compute_region]
+      configuration_aliases = [aws.compute_region, aws.us_east_1]
       source                = "hashicorp/aws"
       version               = "5.15.0"
     }
   }
-}
-
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
 }
 
 data "aws_region" "compute_region" {
