da4c92c0a7409d8b7e27551f2e995a74c0d0996f
Se7enZ <se7enz@protonmail.com>
2025-03-12 13:24:22 -0700

    migrate lambda S3 buckets to remote-state Terraform definition
    This is a preliminary step toward the goal of separating the Lambda
    build procedure from the Terraform infrastructure provisioning.
    Presently, one cannot initially deploy the Terraform infrastructure
    because the AWS S3 buckets containing the Lambda artifacts do not
    exist yet. When the buckets are provisioned separately from the
    rest of the infrastructure, the artifacts can be build an uploaded
    prior running `terraform apply`.

--- deploy/terraform/modules.tf
+++ deploy/terraform/modules.tf
@@ -1,7 +1,20 @@
+# TODO: consider migrating these buckets to direct Terraform variables
+#  instead of lookup based on naming convention. Also consider moving
+#  toward building the lambda functions directly.
+
+data "aws_s3_bucket" "api_lambdas" {
+  bucket = "corecheck-api-lambdas-${terraform.workspace}"
+}
+
+data "aws_s3_bucket" "compute_lambdas" {
+  bucket = "corecheck-compute-lambdas-${terraform.workspace}"
+  provider = aws.compute_region
+}
+
 module "api_gateway" {
   source = "./api-gateway"
 
-  s3_bucket   = aws_s3_bucket.corecheck-lambdas-api.id
+  s3_bucket   = data.aws_s3_bucket.api_lambdas.id
   db_host     = aws_instance.db.public_ip
   db_port     = 5432
   db_user     = var.db_user
@@ -10,11 +23,7 @@ module "api_gateway" {
 
   dns_name = var.dns_name
 
-  corecheck_data_bucket_url = "https://${aws_s3_bucket.bitcoin-coverage-data.id}.s3.${aws_s3_bucket.bitcoin-coverage-data.region}.amazonaws.com"
-
-  providers = {
-    aws.us_east_1 = aws.us_east_1
-  }
+  corecheck_data_bucket_url = "https://${data.aws_s3_bucket.api_lambdas.id}.s3.${data.aws_s3_bucket.api_lambdas.region}.amazonaws.com"
 }
 
 module "compute" {
@@ -37,7 +46,7 @@ module "compute" {
   sonar_token = var.sonar_token
   datadog_api_key = var.datadog_api_key
 
-  lambda_bucket = aws_s3_bucket.corecheck-lambdas.id
+  lambda_bucket = data.aws_s3_bucket.compute_lambdas.id
   providers = {
     aws.us_east_1 = aws.us_east_1
     aws.compute_region = aws.compute_region
